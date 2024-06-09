e688583571559f20087ea0564dd7de69fbf50cdd
demian18 <yaroslav97038@gmail.com>
2024-06-09 14:52:54 +0300

    Add a function to view

--- controllers/index.php
+++ controllers/index.php
@@ -6,4 +6,7 @@ if (isset($_GET['errors'])) {
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 $tasks = $db->query('select * from todo where user_id = 1')->fetchAll(PDO::FETCH_ASSOC);
-require base_path('views/index.view.php');
+view("index.view.php", [
+    'errors' => $errors,
+    'tasks' => $tasks
+]);
--- functions.php
+++ functions.php
@@ -13,3 +13,9 @@ function base_path($path)
 {
     return BASE_PATH . $path;
 }
+
+function view($path, $attributes = [])
+{
+    extract($attributes);
+    require base_path('views/' . $path);
+}
