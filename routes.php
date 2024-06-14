8f40ea734272242e247f2202be0c8c32e66e31bb
demian18 <yaroslav97038@gmail.com>
2024-06-14 17:07:42 +0300

    feat: Add Middleware refactor: Update the methods in Router.php, add return.

--- routes.php
+++ routes.php
@@ -4,8 +4,8 @@ $router->get('/', 'controllers/index.php');
 $router->post('/create-task', 'controllers/create-task.php');
 $router->delete('/delete-task', 'controllers/delete-task.php');
 
-$router->get('/edit-task', 'controllers/edit-task.php');
+$router->get('/edit-task', 'controllers/edit-task.php')->only('auth');
 $router->patch('/update-task', 'controllers/update-task.php');
 
-$router->get('/register', 'controllers/register/create.php');
+$router->get('/register', 'controllers/register/create.php')->only('guest');
 $router->post('/register', 'controllers/register/store.php');
