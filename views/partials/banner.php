3b4c7537e6092ecde2a3f106612704f5ee892b2e
demian18 <yaroslav97038@gmail.com>
2024-06-14 17:35:15 +0300

    feat: Add password_hash refactor: Displaying the name of the registered person and a message for the guest

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,5 +1,10 @@
 <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+            <?php if (!$_SESSION)  : ?>
+                Hello Guest! In theory, you shouldn't see anything here, it will be corrected in the future.
+            <?php elseif ($_SESSION['user']['email']) : ?>
+            Hello <?= $_SESSION['user']['email'] ?>! Welcome to your Task board</h1>
+        <?php endif; ?>
     </div>
 </header>
