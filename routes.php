003ec03976111eeec692477745dfb9097c2b796e
demian18 <yaroslav97038@gmail.com>
2024-06-25 16:09:16 +0300

    refactor(validation): move validation logic to a separate class LoginForm.php
    - Move validation logic from store.php to a separate class.
    
    chore(controllers): organize controllers into 'Http' folder
    
    - Creat a new folder 'Http' and moved controllers into it.

--- routes.php
+++ routes.php
@@ -1,15 +1,15 @@
 <?php
 
-$router->get('/', 'controllers/index.php');
-$router->post('/create-task', 'controllers/create-task.php');
-$router->delete('/delete-task', 'controllers/delete-task.php');
+$router->get('/', '/index.php');
+$router->post('/create-task', '/create-task.php')->only('auth');
+$router->delete('/delete-task', '/delete-task.php')->only('auth');
 
-$router->get('/edit-task', 'controllers/edit-task.php')->only('auth');
-$router->patch('/update-task', 'controllers/update-task.php');
+$router->get('/edit-task', '/edit-task.php')->only('auth');
+$router->patch('/update-task', '/update-task.php')->only('auth');
 
-$router->get('/register', 'controllers/register/create.php')->only('guest');
-$router->post('/register', 'controllers/register/store.php');
+$router->get('/register', '/register/create.php')->only('guest');
+$router->post('/register', '/register/store.php');
 
-$router->get('/login', 'controllers/session/create.php')->only('guest');
-$router->post('/session', 'controllers/session/store.php')->only('guest');
-$router->delete('/session', 'controllers/session/destroy.php')->only('auth');
+$router->get('/login', '/session/create.php')->only('guest');
+$router->post('/session', '/session/store.php')->only('guest');
+$router->delete('/session', '/session/destroy.php')->only('auth');
