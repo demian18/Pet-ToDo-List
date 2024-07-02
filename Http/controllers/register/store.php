1568d5ec316d4f47d349db7c84b95a37972847bc
demian18 <yaroslav97038@gmail.com>
2024-07-02 12:01:22 +0300

    refactor(register controller): move register logic to a separate Registration class

--- Http/controllers/register/store.php
+++ Http/controllers/register/store.php
@@ -1,47 +1,21 @@
 <?php
 
-use Core\App;
-use Core\Database;
-use Core\Validator;
+use Core\Registration;
+use Http\Forms\LoginForm;
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$form = LoginForm::validate($attributes = [
+    'email' => $_POST['email'],
+    'password' => $_POST['password']
+]);
 
-$errors = [];
+$registered = (new Registration)->attempt(
+    $attributes['email'], $attributes['password']
+);
 
-if (!Validator::string($email)) {
-    $errors['email'] = 'Please provide a valid email address.';
+if (!$registered) {
+    $form->error(
+        'email', 'A user with this email already exists.'
+    )->throw();
 }
 
-if (!Validator::string($password, 5, 24)) {
-    $errors['password'] = 'Please provide a password of at least 5 characters.';
-}
-
-if (!empty($errors)) {
-    return view('register/create.view.php', [
-        'errors' => $errors
-    ]);
-}
-
-$db = App::resolve(Database::class);
-
-$user = $db->query('select * from users where email = :email', [
-    'email' => $email
-])->find();
-
-if ($user) {
-    header('location: /');
-    exit();
-} else {
-    $db->query('insert into users(email, password) values(:email, :password)', [
-        'email' => $email,
-        'password' => password_hash($password, PASSWORD_BCRYPT),
-    ]);
-
-    $_SESSION['user'] = [
-        'email' => $email,
-    ];
-
-    header('location: /');
-    exit();
-}
+redirect('/');
